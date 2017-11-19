316db6dd853be8f69b638407d028acc53298e2bf
N8Sayer <forcelord50@gmail.com>
2017-11-19 14:31:05 -0600

    First draft

---
+++ ajax.js
@@ -0,0 +1,41 @@
+var username = 'N8Sayer',
+    password = 'tesser@89';
+
+var settings = {
+  "async": true,
+  "crossDomain": true,
+  "url": "https://wwwcie.ups.com/rest/Track",
+  "method": "POST",
+  "headers": {
+    "content-type": "application/json",
+    "cache-control": "no-cache",
+  },
+  "processData": false,
+  "data": {
+          	"UPSSecurity": {
+          		"UsernameToken": {
+          			"Username":"N8Sayer",
+          			"Password":"tesser@89"
+          		},
+          		"ServiceAccessToken": {
+          			"AccessLicenseNumber":"2D325F978B548248"
+          		}
+          	},
+          	"TrackRequest": {
+          		"Request": {
+          			"RequestOption":"1",
+          			"TransactionReference": {
+          				"CustomerContext": ""
+          			}
+          		},
+          		"InquiryNumber": "1Z7F75Y79073380083"
+          	}
+          }
+};
+
+$.ajax(settings).done(function (response) {
+  $('body').append(result);
+}).fail(function (response) {
+  $('body').append("Failed");
+  console.log(response);
+});
